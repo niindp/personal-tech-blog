450887da542be721b5686ee7503ef2304d5d1457
Nur Iffah Nadhirah <iffah393@gmail.com>
2026-06-01 23:54:39 +0800

    Initial project setup

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const darkModeBtn = document.getElementById("darkModeBtn");
+
+if (darkModeBtn) {
+
+    darkModeBtn.addEventListener("click", () => {
+
+        document.body.classList.toggle("dark-mode");
+
+        if (document.body.classList.contains("dark-mode")) {
+            darkModeBtn.textContent = "☀️ Light Mode";
+        } else {
+            darkModeBtn.textContent = "🌙 Dark Mode";
+        }
+
+    });
+
+}
