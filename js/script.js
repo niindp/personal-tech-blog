6c43e5dea66c4ee5b47dea0f4b90e1153f4e0938
Nur Iffah Nadhirah <iffah393@gmail.com>
2026-06-02 14:56:36 +0800

    Redesigned website interface

--- js/script.js
+++ js/script.js
@@ -1,17 +1,36 @@
 const darkModeBtn = document.getElementById("darkModeBtn");
 
+if (localStorage.getItem("darkMode") === "enabled") {
+    document.body.classList.add("dark-mode");
+}
+
 if (darkModeBtn) {
+
+    updateButtonText();
 
     darkModeBtn.addEventListener("click", () => {
 
         document.body.classList.toggle("dark-mode");
 
         if (document.body.classList.contains("dark-mode")) {
-            darkModeBtn.textContent = "☀️ Light Mode";
+            localStorage.setItem("darkMode", "enabled");
         } else {
-            darkModeBtn.textContent = "🌙 Dark Mode";
+            localStorage.setItem("darkMode", "disabled");
         }
 
+        updateButtonText();
     });
+
+}
+
+function updateButtonText() {
+
+    if (!darkModeBtn) return;
+
+    if (document.body.classList.contains("dark-mode")) {
+        darkModeBtn.textContent = "☀️ Light Mode";
+    } else {
+        darkModeBtn.textContent = "🌙 Dark Mode";
+    }
 
 }
